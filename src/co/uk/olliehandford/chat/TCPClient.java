c31818c38e4c575de001146fa4bf69240c09e911
Ollie Handford <oliverhandford7@gmail.com>
2016-11-16 13:01:41 +0000

    Added project

---
+++ src/co/uk/olliehandford/chat/TCPClient.java
@@ -0,0 +1,22 @@
+package co.uk.olliehandford.chat;
+
+import java.io.*;
+import java.net.*;
+
+class TCPClient
+{
+    public static void main(String argv[]) throws Exception
+    {
+        String sentence;
+        String modifiedSentence;
+
+        BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+        Socket clientSocket = new Socket("localhost", 6789);
+
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        //outToServer.writeBytes(sentence + '\n');
+        clientSocket.close();
+    }
+}
